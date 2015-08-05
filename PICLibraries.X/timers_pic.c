731c417495cb88445d9f1c36080caa41a127f256
Manuel Fernandez <mafernandez4j@hotmail.com>
2015-08-05 14:05:43 +0800

    Added timer0 and related functions

--- PICLibraries.X/timers_pic.c
+++ PICLibraries.X/timers_pic.c
@@ -1,5 +1,31 @@
 #include "timers_pic.h"
 
+void setup_timer0(uint8_t T0CON_reg)
+{
+    T0CON = T0CON_reg;
+}
+
+void set_timer0(int16_t value)
+{
+    INTCONbits.TMR0IF = 0;
+    TMR0 = value;
+}
+
+void timer0_interrupt(bool toggle)
+{
+    if (toggle)
+        INTCON |= 0xC0;
+    
+    INTCONbits.TMR0IE = (toggle) ? 1: 0;
+}
+
+bool timer0_up(void)
+{
+    return (INTCONbits.TMR0IF) ? true : false;
+}
+
+//******************************
+
 void setup_timer3(uint8_t T3CON_reg)
 {
     T3CON = T3CON_reg;
--- PICLibraries.X/timers_pic.h
+++ PICLibraries.X/timers_pic.h
@@ -12,11 +12,31 @@
 #ifndef TIMERS_PIC_H
 #define	TIMERS_PIC_H
 
+#define TMR0_TIMER_ON       0x80
+#define TMR0_8BIT_MODE      0x40
+#define TMR0_EXT_CLOCK      0x20
+#define TMR0_EDGE_SELECT    0x10
+#define TMR0_NO_PRESCALER   0x08
+#define TMR0_PRESCALE_256   0x07
+#define TMR0_PRESCALE_128   0x06
+#define TMR0_PRESCALE_64    0x05
+#define TMR0_PRESCALE_32    0x04
+#define TMR0_PRESCALE_16    0x03
+#define TMR0_PRESCALE_8     0x02
+#define TMR0_PRESCALE_4     0x01
+#define TMR0_PRESCALE_2     0x00
+
+void setup_timer0(uint8_t register_1);
+void set_timer0(int16_t value);
+void timer0_interrupt(bool toggle);
+bool timer0_up(void);
+
 #define TMR3_16BIT_MODE     0x80
 #define TMR3_PRESCALE_8     0x30
 #define TMR3_PRESCALE_4     0x20
 #define TMR3_PRESCALE_2     0x10
 #define TMR3_PRESCALE_1     0x00
+#define TMR3_TO_ECCP        0x80
 #define TMR3_NO_SYNC_CLK    0x04
 #define TMR3_EXT_CLOCK      0x02
 #define TMR3_TIMER_ON       0x01
